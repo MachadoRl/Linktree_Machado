56c0773b395ac9f693db5d03160ca75d2310148e
Rodrigo <rodrigoleemes.2712@gmail.com>
2026-07-06 21:52:38 -0300

    Iniciando Projeto

---
+++ src/components/Card/Index.jsx
@@ -0,0 +1,11 @@
+import { LinkButton } from "../LinkButton/Index"
+import { Profile } from "../Profile/Index"
+import { Footer } from "../Footer/Index"
+
+export function Card() {
+    return(
+        <>
+            
+        </>
+    )
+}
